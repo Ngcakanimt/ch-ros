499e60ca4c4fce323da88bd02854351e5420aa58
Ngcakanimt <mthangakani2@gmail.com>
2018-09-22 20:57:50 +0200

    This is it

---
+++ CSc semester project/CSc semester project/Game mode.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSc_semester_project
+{
+    /// <summary>
+    /// Interaction logic for Game_mode.xaml
+    /// </summary>
+    public partial class Game_mode : Window
+    {
+        Game1 dodge;
+        Game2 shoot;
+        MainWindow menu;
+        public Game_mode()
+        {
+            InitializeComponent();
+        }
+
+        private void Dodge_Click(object sender, RoutedEventArgs e)
+        {
+            dodge = new Game1();
+            dodge.Show();
+            GameMode.Hide();
+           
+        }
+
+        private void Shoot_Click(object sender, RoutedEventArgs e)
+        {
+            shoot = new Game2();
+            shoot.Show();
+            GameMode.Hide();
+        }
+
+        private void Return_Click(object sender, RoutedEventArgs e)
+        {
+            menu = new MainWindow();
+            menu.Show();
+            GameMode.Hide();
+        }
+    }
+}
---
+++ CSc semester project/CSc semester project/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSc_semester_project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Game_mode game;
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            game = new Game_mode();
+            game.Show();
+            Main_menu.Hide();
+
+        }
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to quit?","", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Main_menu.Close();
+            }
+            else if (result == MessageBoxResult.No)
+            {
+                
+            }
+        }
+    }
+}   
